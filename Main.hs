0cd00ccfe0420f8a458f10b6c48d40afaaf1defb
phischu <pschuster@uni-koblenz.de>
2014-01-14 15:30:52 +0100

    display selection

--- Main.hs
+++ Main.hs
@@ -20,7 +20,7 @@ type UID = Integer
 
 renderEditor :: Editor -> Picture
 renderEditor (Editor selection remou) = translate (-500) 250 (pictures [
-    render (guiNest 500 (nestRemou remou)),
+    render (guiNest 500 (nestRemou selection remou)),
     translate 750 (-200) (pictures [
         rectangleWire 500 500,
         renderRemou remou])])
@@ -57,7 +57,7 @@ data Remou a =
 
 data Number a = Number Integer a
 
-data Nest a = Nest String a [Nest a]
+data Nest a = Nest Bool String a [Nest a]
 
 renderRemou :: Remou a -> Picture
 renderRemou (RemouCircle (Number radius _) _) = circle (fromIntegral radius)
@@ -66,19 +66,22 @@ renderRemou (RemouTranslate (Number t1 _) (Number t2 _) remou _) =
 
 type Width = Float
 
-nestRemou :: Remou a -> Nest a
-nestRemou (RemouCircle radius a) = Nest "Circle" a [nestNumber radius]
-nestRemou (RemouTranslate t1 t2 remou a) = Nest "Translate" a [
-    nestNumber t1,
-    nestNumber t2,
-    nestRemou remou]
+nestRemou :: Selection -> Remou UID -> Nest UID
+nestRemou selection (RemouCircle radius uid) = Nest (selection == uid) "Circle" uid [nestNumber selection radius]
+nestRemou selection (RemouTranslate t1 t2 remou uid) = Nest (selection == uid) "Translate" uid [
+    nestNumber selection t1,
+    nestNumber selection t2,
+    nestRemou selection remou]
 
-nestNumber :: Number a -> Nest a
-nestNumber (Number n a) = Nest (show n) a []
+nestNumber :: Selection -> Number UID -> Nest UID
+nestNumber selection (Number n uid) = Nest (selection == uid) (show n) uid []
 
 guiNest :: Width -> Nest a -> GUI a
-guiNest w (Nest caption a nests) = Elements [Pic pic,Click (w,h) a,Position (25,25) (stack (w-25) nests)] where
+guiNest w (Nest selected caption a nests) = Elements [Pic pic,Click (w,h) a,Position (25,25) (stack (w-25) nests)] where
     pic = pictures [
+        if selected
+            then color (light red) (translate (0.5 * w) (-0.5 * h) (rectangleSolid w h))
+            else blank,
         translate (0.5 * w) (-0.5 * h) (rectangleWire w h),
         translate 5 (-20) (scale 0.15 0.15 (text caption))]
     h = 25 + sum (map height nests)
@@ -88,21 +91,21 @@ stack _ [] = Elements []
 stack w (nest:nests) = Elements [guiNest w nest,Position (0,height nest) (stack w nests)]
 
 height :: Nest a -> Float
-height (Nest _ _ nests) = 25 + sum (map height nests)
-
-testgui :: GUI Integer
-testgui = guiNest 300 (nestRemou testast)
+height (Nest _ _ _ nests) = 25 + sum (map height nests)
 
 testast :: Remou UID
 testast =
     RemouTranslate (Number 40 0) (Number 60 1) (RemouCircle (Number 20 4) 3) 2
+
+testeditor :: Editor
+testeditor = Editor 0 testast
 
 main :: IO ()
 main = play
     (InWindow "stuff" (1000,500) (100,100))
     white
     40
-    (Editor 2 testast)
+    testeditor
     renderEditor
     (const id)
     (const id)
